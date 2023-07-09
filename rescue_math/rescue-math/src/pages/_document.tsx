19ba31f8a57c340bce25d71feb1daa0eea0a231f
Adam Wagh <adamwagh@gmail.com>
2023-07-09 16:26:25 -0500

    knocking off a few more

--- rescue_math/rescue-math/src/pages/_document.tsx
+++ rescue_math/rescue-math/src/pages/_document.tsx
@@ -1,14 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         {/* Adding MathJAX */}
-        <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-        <script type="text/javascript" id="MathJax-script" async
+        <Script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></Script>
+        <Script type="text/javascript" id="MathJax-script" async
           src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js">
-        </script>
+        </Script>
       </Head>
       <body>
         <Main />
@@ -17,3 +18,23 @@ export default function Document() {
     </Html>
   );
 }
+
+
+// export default function Document() {
+//   return (
+//     <Html lang="en">
+//       <Head>
+//         {/* Adding MathJAX */}
+//         <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+//         <script type="text/javascript" id="MathJax-script" async
+//           src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js">
+//         </script>
+//       </Head>
+//       <body>
+//         <Main />
+//         <NextScript />
+//       </body>
+//     </Html>
+//   );
+// }
+

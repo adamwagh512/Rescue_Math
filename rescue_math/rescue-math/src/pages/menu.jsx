6d7aec31f6818315ddfabffb720be2fb53e18444
Adam Wagh <adamwagh@gmail.com>
2023-07-09 16:59:12 -0500

    menu done

--- rescue_math/rescue-math/src/pages/menu.jsx
+++ rescue_math/rescue-math/src/pages/menu.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Michroma } from "next/font/google";
-
+import Link from 'next/link'
 //  -------------------------------------------------------------------------------------------------------------
 // Declaring functions so we can use the fonts
 const michroma = Michroma({
@@ -17,42 +17,42 @@ const menu = () => {
         </div>
         <div className={michroma.className}>
           <div className="flex flex-col justify-center items-center">
-            <a href="/weight">
+            <Link href="/weight">
               <button className="bg-blue-800 rounded-lg p-5 mt-10 w-[75vw]">
                 Weight Conversion
               </button>
-            </a>
-            <a href="/fluidBolus">
+            </Link>
+            <Link href="/fluidBolus">
               <button className="bg-blue-800 rounded-lg p-5 mt-10 w-[75vw]">
                 IV Fluid Administration
               </button>
-            </a>
-            <a href="/bolus">
+            </Link>
+            <Link href="/bolus">
               <button className="bg-blue-800 rounded-lg p-5 mt-10 w-[75vw]">
                 IV Medication Bolus
               </button>
-            </a>
-            <a href="/">
+            </Link>
+            <Link href="/">
               <button className="bg-gray-800 rounded-lg p-5 mt-10 w-[75vw]">
                 IV Medication Drip
               </button>
-            </a>
-            <a href="/Parkland">
+            </Link>
+            <Link href="/Parkland">
               <button className="bg-blue-800 rounded-lg p-5 mt-10 w-[75vw]">
                 Parkland Burn Formula
               </button>
-            </a>
-            <a href="/weightMedBolus"><button className="bg-blue-800 rounded-lg p-5 mt-10 w-[75vw]">
+            </Link>
+            <Link href="/weightMedBolus"><button className="bg-blue-800 rounded-lg p-5 mt-10 w-[75vw]">
               Weight-Based Med Bolus
-            </button></a>
-            <a href="/weightFluid"><button className="bg-blue-800 rounded-lg p-5 mt-10 w-[75vw]">
+            </button></Link>
+            <Link href="/weightFluid"><button className="bg-blue-800 rounded-lg p-5 mt-10 w-[75vw]">
               Weight-Based Fluid Drip
-            </button></a>
-            <a href="/Homepage">
+            </button></Link>
+            <Link href="/Homepage">
               <button className="bg-red-800 rounded-lg p-5 mt-10 w-[75vw]">
                 Return to Home
               </button>
-            </a>
+            </Link>
           </div>
         </div>
       </div>
